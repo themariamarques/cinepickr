53393c3cee64722efaaab793b2814d318515d8cf
Maria Marques <mariamarques@MacBook-Pro-de-Maria.local>
2020-02-08 22:40:51 +0000

    feat: Add youtuber player modal

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -5,6 +5,7 @@ import Styles from "./Poster.module.css";
 
 const Poster = ({ film }) => {
   const { poster_path: path, videos, title } = film;
+
   const basePath = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
 
   return (
--- src/components/TrailerPlay/TrailerPlay.js
+++ src/components/TrailerPlay/TrailerPlay.js
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import IconPlay from "../IconPlay";
 import Styles from "./TrailerPlay.module.css";
+import Modal from "react-modal";
+import YouTube from "react-youtube";
 
 const TrailerPlay = ({ videos }) => {
+  const [isTrailerModalOpen, setTrailerModalOpen] = useState(false);
   const trailer = findTrailer(videos);
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      minHeight: "390px",
+      minWidth: "640px",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)"
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1
+    }
+  };
 
   if (!trailer) {
     return null;
   }
 
   return (
-    <a
-      className={Styles.link}
-      href={`https://www.youtube.com/watch?v=${trailer.key}`}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <IconPlay />
-    </a>
+    <>
+      <Modal
+        isOpen={isTrailerModalOpen}
+        onRequestClose={() => setTrailerModalOpen(false)}
+        overlayClassName={Styles.modalOverlay}
+        style={customStyles}
+      >
+        <YouTube videoId={trailer.key} opts={opts} />
+      </Modal>
+      <div
+        className={Styles.posterOverlay}
+        href={`https://www.youtube.com/watch?v=${trailer.key}`}
+        onClick={() => setTrailerModalOpen(true)}
+        role="button"
+      >
+        <IconPlay />
+      </div>
+    </>
   );
 };
 
